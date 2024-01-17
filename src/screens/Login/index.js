7f202b369fb6cba29aa4c9aa5332807e771b333c
shibz1998 <miahshiblu1998@gmail.com>
2024-01-17 16:58:34 +0000

    Login Input & Styles Sorted

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,7 +1,6 @@
 import {
   StyleSheet,
   Text,
-  TextInput,
   TouchableOpacity,
   View,
   Alert,
@@ -10,31 +9,36 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import { FIREBASE_AUTH } from "../../../FirebaseConfig";
-
+import InputComponent from "../../components/InputComponent";
+import { useForm, FieldValues } from "react-hook-form";
 import { signInWithEmailAndPassword } from "firebase/auth";
+
+import styles from "../../styles/AuthStyles";
 
 export default function Login(props) {
   console.log("Login Page Rendering");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   const auth = FIREBASE_AUTH;
 
-  const handleLogin = async () => {
+  const handleLogin = async (data) => {
+    const { Email, Password } = data;
+
+    console.log(Email, Password);
     try {
-      // Validate email and password
-      if (!email || !password) {
+      if (!Email || !Password) {
         Alert.alert("Error", "Please enter both email and password");
         return;
       }
-
-      // Sign in with email and password
-      await signInWithEmailAndPassword(auth, email, password);
-
-      // Successfully logged in
+      await signInWithEmailAndPassword(auth, Email, Password);
       console.log("User logged in successfully");
     } catch (error) {
-      // Handle login errors
       console.error("Login Error: ", error.message);
       Alert.alert("Error", "Invalid email or password. Please try again.");
     }
@@ -51,25 +55,27 @@ export default function Login(props) {
 
         <Text style={styles.title}>Login</Text>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={(t) => {
-            setEmail(t);
-          }}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          secureTextEntry={true}
-          onChangeText={(t) => {
-            setPassword(t);
-          }}
+        <InputComponent
+          control={control}
+          placeholder={"Email"}
+          name="Email"
+          error={errors?.Email}
+          autoCapitalize="none"
         />
 
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <InputComponent
+          control={control}
+          placeholder={"Password"}
+          name="Password"
+          error={errors?.Password}
+          autoCapitalize="none"
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(handleLogin)}
+        >
           <Text style={styles.buttonTextColor}>Login</Text>
         </TouchableOpacity>
 
@@ -82,70 +88,70 @@ export default function Login(props) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
 
-  title: {
-    fontWeight: "bold",
-    fontSize: 30,
-    // color: "#d04531",
-    color: "#94c11e",
+//   title: {
+//     fontWeight: "bold",
+//     fontSize: 30,
+//     // color: "#d04531",
+//     color: "#94c11e",
 
-    marginVertical: 10,
-  },
+//     marginVertical: 10,
+//   },
 
-  button: {
-    backgroundColor: "#212121",
+//   button: {
+//     backgroundColor: "#212121",
 
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-    margin: 10,
-    marginTop: 15,
-  },
-  input: {
-    borderRadius: 5,
-    padding: 10,
-    margin: 10,
-    backgroundColor: "white",
-    // opacity: 0.9,
-    // zIndex: 0,
+//     borderRadius: 10,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: 10,
+//     margin: 10,
+//     marginTop: 15,
+//   },
+//   input: {
+//     borderRadius: 5,
+//     padding: 10,
+//     margin: 10,
+//     backgroundColor: "white",
+//     // opacity: 0.9,
+//     // zIndex: 0,
 
-    width: 250,
-  },
-  buttonTextColor: {
-    color: "white",
-  },
-  card: {
-    backgroundColor: "#e4ede7",
+//     width: 250,
+//   },
+//   buttonTextColor: {
+//     color: "white",
+//   },
+//   card: {
+//     backgroundColor: "#e4ede7",
 
-    alignItems: "center",
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 5,
-  },
+//     alignItems: "center",
+//     borderRadius: 10,
+//     padding: 20,
+//     marginBottom: 10,
+//     shadowColor: "#000",
+//     shadowOffset: {
+//       width: 0,
+//       height: 2,
+//     },
+//     shadowOpacity: 0.2,
+//     shadowRadius: 3,
+//     elevation: 5,
+//   },
 
-  text: {
-    color: "black", // Adjust the color according to your design
-    // textAlign: 'center',
-    marginTop: 10,
-  },
-  linkText: {
-    color: "#d04531", // Orange color
-    textDecorationLine: "underline",
-  },
-});
+//   text: {
+//     color: "black", // Adjust the color according to your design
+//     // textAlign: 'center',
+//     marginTop: 10,
+//   },
+//   linkText: {
+//     color: "#d04531", // Orange color
+//     textDecorationLine: "underline",
+//   },
+// });
