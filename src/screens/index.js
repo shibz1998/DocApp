2bf6570c33a33a1a1521804e06d8c18236b1b558
shibz1998 <miahshiblu1998@gmail.com>
2024-01-19 17:53:03 +0000

    Working Auth Code

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,5 @@
 import Login from "./Login";
 import SignUp from "./SignUp";
-
 import DocDashboardScreen from "./DocScreen/DashboardScreen";
 import PatientDashboardScreen from "./PatientScreen/DashboardScreen";
-
 export { Login, SignUp, DocDashboardScreen, PatientDashboardScreen };
