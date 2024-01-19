0448503d89cae6cccb16e142984358fd11b1eac8
shibz1998 <miahshiblu1998@gmail.com>
2024-01-19 23:55:22 +0000

    persist Working

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,4 +1,8 @@
 // Import the functions you need from the SDKs you need
+
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -19,5 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
