bb94b1e7e5fdd322e96d7456cd6510a070caf9bd
shibz1998 <miahshiblu1998@gmail.com>
2024-01-22 00:14:42 +0000

    Update useFirestore.js

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -56,33 +56,6 @@ export const useFirestore = () => {
     return unsubscribe;
   };
 
-  const listenToDocument = (collectionName, userId, onUpdate, onError) => {
-    const q = query(
-      collection(FIRESTORE_DB, collectionName),
-      where("userId", "==", userId)
-    );
-    const unsubscribe = onSnapshot(
-      q,
-      (querySnapshot) => {
-        if (!querySnapshot.empty) {
-          const docs = querySnapshot.docs.map((doc) => doc.data());
-          onUpdate(docs);
-        } else {
-          console.log("No documents found.");
-          onUpdate([]);
-        }
-      },
-      (error) => {
-        console.error("Error listening to document:", error);
-        if (onError) {
-          onError(error);
-        }
-      }
-    );
-
-    return unsubscribe;
-  };
-
   const listenToDoctorProfiles = (onUpdate, onError) => {
     const collectionRef = collection(FIRESTORE_DB, "UserProfile");
     const q = query(collectionRef, where("userType", "==", "doctor"));
@@ -102,30 +75,36 @@ export const useFirestore = () => {
     return unsubscribe;
   };
 
-  // const getDocument = async (userId, collectionName) => {
-  //   try {
-  //     const docRef = doc(FIRESTORE_DB, collectionName, userId);
-  //     const docSnap = await getDoc(docRef);
-
-  //     if (docSnap.exists()) {
-  //       console.log("Document data:", docSnap.data());
-  //       return docSnap.data();
-  //     } else {
-  //       console.log("No such document!");
-  //       return null;
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching user profile:", error);
-  //     throw error;
-  //   }
-  // };
-
-  // More Firestore operations like update, delete can be added here
-
   return {
     addDocument,
     getDocument,
-    listenToDocument,
     listenToDoctorProfiles,
   };
 };
+
+// const listenToDocument = (collectionName, userId, onUpdate, onError) => {
+//   const q = query(
+//     collection(FIRESTORE_DB, collectionName),
+//     where("userId", "==", userId)
+//   );
+//   const unsubscribe = onSnapshot(
+//     q,
+//     (querySnapshot) => {
+//       if (!querySnapshot.empty) {
+//         const docs = querySnapshot.docs.map((doc) => doc.data());
+//         onUpdate(docs);
+//       } else {
+//         console.log("No documents found.");
+//         onUpdate([]);
+//       }
+//     },
+//     (error) => {
+//       console.error("Error listening to document:", error);
+//       if (onError) {
+//         onError(error);
+//       }
+//     }
+//   );
+
+//   return unsubscribe;
+// };
